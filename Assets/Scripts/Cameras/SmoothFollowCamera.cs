8f5e04c258b939549697015645df0b523fe42e2b
James L. Walsh <jloubierwalsh@gmail.com>
2018-09-09 16:53:06 -0400

    Tweaked the SmoothFollowCamera

--- Assets/Scripts/Cameras/SmoothFollowCamera.cs
+++ Assets/Scripts/Cameras/SmoothFollowCamera.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 [RequireComponent(typeof (Rigidbody))]
 public class SmoothFollowCamera : MonoBehaviour {
 
+    private enum LookDirection { BEHIND, FORWARD, LEFT, RIGHT };
+
     public Camera targetCamera;
     public Vector3 minOffset;
     public Vector3 maxOffset;
+    public float velocityAtMaxOffset;
 
     private Rigidbody targetRigidbody;
 
@@ -19,12 +22,43 @@ public class SmoothFollowCamera : MonoBehaviour {
     void Update () {
         float speed = targetRigidbody.velocity.magnitude;
 
-        Vector3 speedOffset = Vector3.Lerp(minOffset, maxOffset, speed / 100f);
+        Vector3 speedOffset = Vector3.Lerp(minOffset, maxOffset, speed / velocityAtMaxOffset);
+        Vector3 offsetWithRotation = GetCameraRotation() * speedOffset;
 
-        Vector3 offsetWithRotation = Quaternion.LookRotation(transform.forward, transform.up) * speedOffset;
-
-
-        targetCamera.transform.position = offsetWithRotation + transform.position;
+        targetCamera.transform.position = transform.position + offsetWithRotation;
         targetCamera.transform.LookAt(transform, transform.up);
 	}
+
+    private Quaternion GetCameraRotation()
+    {
+       switch(GetLookDirection())
+        {
+            case LookDirection.FORWARD:
+                return Quaternion.LookRotation(-transform.forward, transform.up);
+            case LookDirection.RIGHT:
+                return Quaternion.LookRotation(-transform.right, transform.up);
+            case LookDirection.LEFT:
+                return Quaternion.LookRotation(transform.right, transform.up);
+            case LookDirection.BEHIND:
+            default:
+                return Quaternion.LookRotation(transform.forward, transform.up);
+        }
+    }
+
+    private LookDirection GetLookDirection()
+    {
+        bool lookLeft = Input.GetButton("LookLeft");
+        bool lookRight = Input.GetButton("LookRight");
+
+        if (lookLeft && lookRight)
+            return LookDirection.FORWARD;
+
+        if (lookLeft)
+            return LookDirection.LEFT;
+
+        if (lookRight)
+            return LookDirection.RIGHT;
+
+        return LookDirection.BEHIND;
+    }
 }
