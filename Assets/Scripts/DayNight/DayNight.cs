8c17c1aaabb66603a193c718452efa648a9942d6
B533P-25\533 <pellthomas@hotmail.com>
2018-09-28 16:03:17 -0400

    Cycle de jour et de nuit automatique fait

---
+++ Assets/Scripts/DayNight/DayNight.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, 5f * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+	}
+}
