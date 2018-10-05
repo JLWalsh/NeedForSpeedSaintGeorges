e5274d7c73f438b409ea8d211b129b93734a2d35
B533P-17\533 <pellthomas@hotmail.com>
2018-10-05 16:00:09 -0400

    Affichage des etoiles fait

---
+++ Assets/Scripts/DayNight/Stars.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stars : MonoBehaviour {
+
+    public ParticleSystem etoiles;
+    public Light sun;
+
+	// Use this for initialization
+	void Start () {
+        etoiles.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(Vector3.zero, Vector3.right, 5f * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+        if (sun.transform.eulerAngles.x <= 91)
+        {
+            etoiles.Stop());
+        }
+        else if (sun.transform.eulerAngles.x > 91)
+        {
+            etoiles.Play();
+        }
+    }
+
+    void AfficherEtoiles()
+    {
+
+    }
+
+    void EffacerEtoiles()
+    {
+
+    }
+}
