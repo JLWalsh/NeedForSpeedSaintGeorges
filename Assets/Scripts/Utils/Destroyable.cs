be4de0d1cdbd888182a92ab0ff5b3877a38c9f39
James L. Walsh <jloubierwalsh@gmail.com>
2018-10-13 14:40:17 -0400

    Continued working on Saint-Georges

---
+++ Assets/Scripts/Utils/Destroyable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Destroyable : MonoBehaviour {
+
+    private static readonly string DESTROY_FOR_TAG = "Player";
+
+    private void Awake() {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        if (rigidbody != null)
+            Destroy(rigidbody);
+
+        BoxCollider boxCollider = GetComponent<BoxCollider>();
+
+        if (boxCollider == null)
+            gameObject.AddComponent<BoxCollider>();
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.rigidbody == null)
+            return;
+
+        if (collision.rigidbody.tag == DESTROY_FOR_TAG) {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+            if(rigidbody == null) {
+                rigidbody = gameObject.AddComponent<Rigidbody>();
+            }
+
+            rigidbody.AddForce(collision.relativeVelocity * 100f);
+        }
+    }
+
+}
