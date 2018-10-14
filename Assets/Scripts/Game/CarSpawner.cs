690fed0996cd342da465ea736353d8b2d3c7b673
James L. Walsh <jloubierwalsh@gmail.com>
2018-10-13 20:44:15 -0400

    Started working on car selection

---
+++ Assets/Scripts/Game/CarSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarSpawner : MonoBehaviour {
+
+    private void Awake() {
+        CarSpawner[] spawners = FindObjectsOfType<CarSpawner>();
+
+        if(spawners.Length > 1) {
+            Destroy(this);
+        } else {
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void Spawn(GameObject prefab) {
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+}
