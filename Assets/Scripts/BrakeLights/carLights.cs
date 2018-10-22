a00eef5b072c4584c485186e57294ffcc1fd2c8c
DESKTOP-ICV09IO\Thomas <pellthomas@hotmail.com>
2018-10-21 22:33:47 -0300

    Debut lumiere arriere

---
+++ Assets/Scripts/BrakeLights/carLights.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carLights : MonoBehaviour {
+
+    public Renderer brakeLights;
+    public Material brakeLightOn;
+    public Material brakeLightOff;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Space))
+        {
+            brakeLights.material = brakeLightOn;
+        }
+        else
+        {
+            brakeLights.material = brakeLightOff;
+        }
+	}
+}
