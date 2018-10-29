b29ff4396157c0fba84b8dd35dc541ae4962d4f7
B533P-18\533 <pellthomas@hotmail.com>
2018-10-29 16:38:44 -0400

    Vitesse nitro faite

---
+++ Assets/Scripts/Nitro/Vitesse.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitesse : MonoBehaviour {
+
+    public float vitesseNitro;
+    public float tempsRecharge;
+    public float tempsRestant;
+    public bool nitroEnable;
+    public Rigidbody voiture;
+	// Use this for initialization
+	void Start () {
+        vitesseNitro = 20000;
+        tempsRecharge = 100;
+        tempsRestant = 1000;
+        nitroEnable = true;
+        voiture.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (nitroEnable == true)
+            {
+                UtiliserNitro();
+            }
+        }
+        if (tempsRecharge == 0)
+        {
+            nitroEnable = false;
+            tempsRestant--;
+            print(tempsRestant);
+            if (tempsRestant == 0)
+            {
+                nitroEnable = true;
+                tempsRecharge = 100;
+                tempsRestant = 1000;
+            }
+        }
+	}
+
+    void UtiliserNitro()
+    {
+       tempsRecharge--;
+       print(tempsRecharge);
+       voiture.AddForce(transform.forward * vitesseNitro);
+    }
+}
